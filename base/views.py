4c5a9d7292859ae882df8fef7ad91f18fac87d1c
Kevin <kmgoldfarb@gmail.com>
2021-11-09 16:47:37 -0600

    Implementing halfmoon CSS framework
    Implementing the halfmoon CSS framework to style the Django templates.

--- base/views.py
+++ base/views.py
@@ -3,12 +3,6 @@ from django.db.models import Q
 from .models import Article
 from .scrapers import get_autosport, get_wtf1
 
-# articles = [
-#    {'id': 1, 'title': 'Verstappen Wins Mexico'},
-#    {'id': 2, 'title': 'Bottas Leaves Too Much Room'},
-#    {'id': 3, 'title': 'Perez Scores Home Podium'},
-# ] 
-
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -21,16 +15,19 @@ def home(request):
     for s in sites:
         formatted_sites.append(s[0])
     context =  {'articles': sorted_articles, 'sites': formatted_sites}
-    return render(request, 'base/home.html', context)
+    return render(request, 'base/main.html', context)
+
 
 def article(request, pk):
     article = Article.objects.get(id=pk)
     context = {'article': article}
     return render(request, 'base/article.html', context)
 
+
 def standings(request):
     # get_autosport()
     return render(request, 'base/standings.html')
+
 
 def upcoming(request):
     # get_wtf1()
